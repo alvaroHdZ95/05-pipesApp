e33aac521ea0ef2195ce9162d5bbe19e2d6ad8e1
VN - Alvaro Hdz Valero <alvaro.hernandezv@viewnext.com>
2024-02-12 11:25:26 +0100

    S11 - Instalar PrimeNG

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PrimeNGConfig } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   public title = 'álVaRO heRnÁNdEZ VAlErO';
+
+  constructor(private primeConfig: PrimeNGConfig) {}
+
+  ngOnInit(): void {
+    this.primeConfig.ripple = true;
+  }
 }
