f34d2164f2c8358a86dd019eeb3d8bc9a3566300
VN - Alvaro Hdz Valero <alvaro.hernandezv@viewnext.com>
2024-02-13 11:02:21 +0100

    S11 - PrimeNG - MenuBar

--- src/app/prime-ng/prime-ng.module.ts
+++ src/app/prime-ng/prime-ng.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 
 import { MenuModule } from 'primeng/menu';
+import { MenubarModule } from 'primeng/menubar';
 
 //* El objetivo de este módulo es exportar todos los módulos que voy a necesitar
 
 @NgModule({
   declarations: [],
   exports: [
+      MenubarModule,
       MenuModule,
     ]
 })
