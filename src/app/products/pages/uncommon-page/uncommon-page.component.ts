4ee40b9a8de06a5542f737b1253b3e37b49f4ae5
VN - Alvaro Hdz Valero <alvaro.hernandezv@viewnext.com>
2024-02-14 12:45:04 +0100

    S11 - |18nPluralPipe

--- src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -7,17 +7,29 @@ import { Component } from '@angular/core';
 })
 export class UncommonPageComponent {
 
-  //* i18n Select
+  //* i18nSelect
   public name: string = 'Fernando';
   public gender: 'male' | 'female' = 'male';
   public invitationMap = {
     male: 'invitarlo',
-    female: 'invitarla',
+    female: 'invitarla'
   }
 
   changeClient(): void {
     this.name = 'Melissa';
     this.gender = 'female';
+  }
+
+  //* i18nPlural
+  public clients: string[] = ['María', 'Pedro', 'Fernando', 'Eduardo', 'Melissa'];
+  public clientsMap = {
+    '=0': 'no tenemos ningún cliente esperando.',
+    '=1': 'tenemos # cliente esperando.',
+    'other': 'tenemos # clientes esperando.'
+  }
+
+  deleteClient(): void {
+    this.clients.shift();
   }
 
 }
